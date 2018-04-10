9a668b68e46d9c1b24c81e7e38955f3d36c06a96
Florence Schwartz <schflo@gmail.com>
2018-04-10 15:21:58 +1000

    finished exercice

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+
+  def index
+    @task = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get "tasks", to: "tasks#index", as: :tasks
+
+post "tasks", to: "tasks#create"
+get "tasks/new", to: "tasks#new", as: :new_task
+
+get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+get "tasks/:id", to: "tasks#show", as: :task
+
+patch "tasks/:id", to: "tasks#update"
+
+delete "tasks/:id", to: "tasks#destroy"
+
 end
